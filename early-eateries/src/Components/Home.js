a4b56318b6c7c5d15e28939e4e4ec54a5950093c
akash mishra <akashmishra1128@gmail.com>
2023-05-07 14:15:07 +0530

    added more data

--- early-eateries/src/Components/Home.js
+++ early-eateries/src/Components/Home.js
@@ -12,12 +12,14 @@ import {
   ListGroupItem,
 } from "reactstrap";
 import "./Home.css";
-import data from "./../Store/data.json";
-import image from "./../Store/Photos/vaidyaMisal.jpg";
+import { earlyeateries } from "../Store/data.js";
+import image0 from "./../Store/Photos/vaidyaMisal.jpg";
+import image1 from "./../Store/Photos/mutton_cutlet.jpg";
+import image2 from "./../Store/Photos/cold_drink.jpg";
 export default function Home() {
-  const eateryList = data.earlyeateries;
+  const eateryList = earlyeateries;
   console.log(eateryList);
-
+  const bgColor = "#E7E8D1";
   const getEateryCardList = (List) => {
     return (
       <div class="eatery">
@@ -28,30 +30,34 @@ export default function Home() {
             backgroundColor: "white",
           }}
         >
-          {List.map((eatery) => {
+          {List.map((eatery, index) => {
             console.log("eatry", eatery);
             return (
               <Card
                 style={{
                   display: "flex",
                   flexDirection: "row",
-                  backgroundColor: "#E7E8D1",
+                  backgroundColor: bgColor,
+                  width: "150%",
                   margin: "0.8rem",
                 }}
               >
                 <CardImg
                   alt="Card image cap"
-                  src={image}
+                  src={eatery.image}
                   top
                   height={300}
-                  width="100%"
-                  style={{ width: "50%" }}
+                  width="40%"
+                  style={{
+                    width: 500,
+                    objectFit: "cover",
+                  }}
                 />
                 <CardBody>
                   <CardTitle
                     tag="h5"
                     style={{
-                      backgroundColor: "#E7E8D1",
+                      backgroundColor: bgColor,
                       fontFamily: "Segoe UI",
                     }}
                   >
@@ -63,12 +69,12 @@ export default function Home() {
                   <ListGroup
                     flush
                     style={{
-                      backgroundColor: "#E7E8D1",
+                      backgroundColor: bgColor,
                     }}
                   >
                     <ListGroupItem
                       style={{
-                        backgroundColor: "#E7E8D1",
+                        backgroundColor: bgColor,
                       }}
                     >
                       <b style={{ fontFamily: "MV Boli" }}>Dish - </b>{" "}
@@ -76,7 +82,7 @@ export default function Home() {
                     </ListGroupItem>
                     <ListGroupItem
                       style={{
-                        backgroundColor: "#E7E8D1",
+                        backgroundColor: bgColor,
                       }}
                     >
                       <b style={{ fontFamily: "MV Boli" }}>Price - </b>{" "}
@@ -84,7 +90,7 @@ export default function Home() {
                     </ListGroupItem>
                     <ListGroupItem
                       style={{
-                        backgroundColor: "#E7E8D1",
+                        backgroundColor: bgColor,
                       }}
                     >
                       <b style={{ fontFamily: "MV Boli" }}>Location - </b>{" "}
@@ -92,7 +98,7 @@ export default function Home() {
                     </ListGroupItem>
                     <ListGroupItem
                       style={{
-                        backgroundColor: "#E7E8D1",
+                        backgroundColor: bgColor,
                       }}
                     >
                       <b style={{ fontFamily: "MV Boli" }}>Date - </b>{" "}
@@ -109,7 +115,7 @@ export default function Home() {
   };
   return (
     <div>
-      <div class="d-flex" style={{}}>
+      <div class="d-flex" style={{ width: "100%" }}>
         <div class="leftpane"></div>
         <div class="middlepane">{getEateryCardList(eateryList)}</div>
         <div class="rightpane"></div>
---
+++ early-eateries/src/Store/data.js
@@ -0,0 +1,37 @@
+export const earlyeateries = [
+  {
+    eateryName: "Vaidya Uphar Gruha",
+    dishName: "Misal Pav",
+    price: " ₹ 80 -100",
+    location: "676, Bagade Rd, Budhwar Peth, Pune, Maharashtra 411002",
+    date: " 27 Nov 2022",
+    menu: "",
+    image: require("./Photos/vaidyaMisal.jpg"),
+    WebsiteLink:
+      "https://www.google.com/maps/place/Vaidya+Upahar+Gruha/@18.517934,73.8593956,15z/data=!4m6!3m5!1s0x3bc2c06f5370d2f1:0x95e48b12e6c9f71e!8m2!3d18.517934!4d73.8593956!16s%2Fg%2F12q4vz2s8",
+  },
+  {
+    eateryName: "Dorabjee and Sons",
+    dishName: "Mutton Sami Kabab",
+    price: " ₹ 60-80",
+    location:
+      "Sharbat Wala, 845, Dastur Meher Road, Chowk, Pune, Maharashtra 411001",
+    date: " 07 May 2022",
+    menu: "",
+    image: require("./Photos/mutton_cutlet.jpg"),
+    WebsiteLink:
+      "https://www.google.com/maps/dir/18.557961,73.7830858/dorabjee+and+sons/@18.5381258,73.7613649,12z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bc2c04f7dfab193:0xa8add9a5d3292e2b!2m2!1d73.877045!2d18.515024?hl=en",
+  },
+  {
+    eateryName: "Dorabjee and Sons",
+    dishName: "Ardeshir Rasberry/Lemon",
+    price: " ₹ 40-50",
+    location:
+      "Sharbat Wala, 845, Dastur Meher Road, Chowk, Pune, Maharashtra 411001",
+    date: " 07 May 2022",
+    menu: "",
+    image: require("./Photos/cold_drink.jpg"),
+    WebsiteLink:
+      "https://www.google.com/maps/dir/18.557961,73.7830858/dorabjee+and+sons/@18.5381258,73.7613649,12z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bc2c04f7dfab193:0xa8add9a5d3292e2b!2m2!1d73.877045!2d18.515024?hl=en",
+  },
+];
